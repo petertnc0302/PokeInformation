798bf3706731d3be619003ac37cd2056e16a71aa
nguyencuong <tncuong0302@gmail.com>
2024-06-19 14:53:06 +0700

    Initial Commit

---
+++ PokeInformation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeInformation
+//
+//  Created by Nguyen Cuong on 19/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
